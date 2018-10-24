3837e776bec7ee671b79dc9ad9f07bbf5bcf6c3f
Atsushi <ahasimoto@mm.media.kyoto-u.ac.jp>
2018-10-24 18:45:34 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages, Extension
+from hyper_feature_coding import __author__, __version__, __license__
+ 
+setup(
+        name             = 'lab-note',
+        version          = __version__,
+        description      = '.',
+        license          = __license__,
+        author           = __author__,
+        author_email     = 'atsushi.hashimoto@sinicx.com',
+        url              = 'https://AtsushiHashimoto.github.io/lab-note',
+        keywords         = 'laboratory note, experiments, archive',
+        packages         = find_packages(),
+	include_package_data = True,
+        install_requires = ['easydict','json','yaml'],
+        )
+ 
