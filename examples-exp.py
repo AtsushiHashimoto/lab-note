0f734da9b34de0b569323f00afbeaa74d1bfd86e
AtsushiHashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2019-01-18 15:28:15 +0900

    refine example code.

--- examples-exp.py
+++ examples-exp.py
@@ -14,57 +14,61 @@ import labnote as lb
 
 
 # # 2. load yaml for argument setting
-# The following code is just to produce yaml in code. You can prepare it manually.
+# The following configuration is equivalent to write...
+# 
+#     parser.add_argument('--N',default=100,type=int,help='# of samples')
+#     parser.add_argument('--alpha', '-a', default=1.0,type=float,help='std. dev. of the first normal distribution')
+#     parser.add_argument('--beta', '-b', default=1.0, type=float,help='std. dev. of the second normal distribution')
+# 
 
 # In[2]:
 
 
 import yaml
 from labnote.utils import register_arg4yaml as reg
-# Create an example of yaml to set command-line arguments.
-# Any args for add_argument function is available.
-# You can create the file by editting the yaml file directly.
-default_args = {
-    'alpha':reg(1.0,'-a',help='std. dev. of the first normal distribution'),
-    'beta':reg(1.0,'-b',help='std. dev. of the second normal distribution'),
-    'N':reg(100,'-N',int,help='# of samples'),    
-}
+
 default_args_yaml = './example_default_args.yml'
+# all options of add_argument function can be a key of each parameter entry.
+with open(default_args_yaml,'w') as f:
+    conf = '''
+N:
+  default: 100
+  type: int
+  help: '# of samples'
+alpha:
+  name: -a
+  default: 1.0
+  type: float
+  help: std. dev. of the first normal distribution
+beta:
+  name: -b
+  default: 1.0
+  type: float
+  help: std. dev. of the second normal distribution
+'''
+    f.write(conf)
 
-#with open(default_args_yaml,'w') as f:
-#    f.write(yaml.dump(default_args, default_flow_style=False))
-
-
-# ### example_default_args.yml
-# 
-#     N:
-#       default: 100
-#       help: '# of samples'
-#       type: int
-#     alpha:
-#       default: 1.0
-#       help: std. dev. of the first normal distribution
-#       type: float
-#     beta:
-#       default: 1.0
-#       help: std. dev. of the second normal distribution
-#       type: float
 
 # # 3. Feed yaml from command line
-# The following code is just to produce config file in code. You can prepare it manually.
-# To feed config file (from command line), use --config or -c option.
+# The following configuration can be fed to your code by...
+# 
+#     % python your_code.py --config ./test.yml
+#     
+# And it is equivalent, in this case, to...
+# 
+#     % python your_code.py --alpha 2.0 --beta 0.5 
 
 # In[3]:
 
 
-# Create an example of an experimental configulation.
-# You can create the file by editting the yaml file directly.
-config_args = {
-    'beta':0.5
-}
 config_yaml = './test.yml'
 with open(config_yaml,'w') as f:
-    f.write(yaml.dump(config_args, default_flow_style=False))
+    f.write(
+'''
+alpha: 2.0
+beta: 0.5
+'''
+)
 
 
 # # 4. Create a note.
@@ -72,14 +76,25 @@ with open(config_yaml,'w') as f:
 # In[4]:
 
 
-note = lb.Note('./log',arguments=[default_args_yaml,{'foo':1}])
+note = lb.Note('./log',arguments=[default_args_yaml])
+print(note.params)
+
+
+# ### simulate a process call with --config option.
+# 
+#     % python your_code.py --config ./test.yml
+
+# In[5]:
+
+
+note = lb.Note('./log',arguments=[default_args_yaml,{'config':config_yaml}])
 print(note.params)
 
 
 # # 5. Save experimental results (in two ways)
 # 'note.record()' makes result directory with timestamp.
 
-# In[5]:
+# In[6]:
 
 
 import os.path
@@ -110,7 +125,7 @@ rec = note.record()
 # # 6. close session
 # exit() calls note destructor, which save the jupyter log as an .html file in the `exp_log' directory.
 
-# In[6]:
+# In[7]:
 
 
 note.wrapup()
