5e8b64167957819c32f4842650e31ef778c2c670
Atsushi Hashimoto <atsushi.hashimoto@sinicx.com>
2019-01-29 16:33:02 +0900

    fix a bug of setup conf.

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
         keywords         = 'laboratory note, experiments, archive',
         packages         = find_packages(),
 	include_package_data = True,
-        install_requires = ['pipreqs','easydict','pyyaml','jupyter-notebook','requests'],
+        install_requires = ['pipreqs','easydict','pyyaml','jupyter','requests'],
         )
  
